3a56a414f324a8c8637e82587401ad22d0ebb8fd
Shaofei Li <31947328+SFL012@users.noreply.github.com>
2023-03-06 21:12:31 -0500

    Update Kalman_Filter.py

--- Trading/Kalman_Filter.py
+++ Trading/Kalman_Filter.py
@@ -1,6 +1,4 @@
-""" Python For Quantitative Finance                                             """ 
 """ Dynamic Hedge Ratio Between ETF Pairs Using the Kalman Filter               """
-""" Shaofei Li                                                                  """
 
 from __future__ import print_function
 
@@ -20,27 +18,31 @@ def draw_date_coloured_scatterplot(etfs, prices):
     # Create a yellow-to-red colourmap where yellow indicates
     # early dates and red indicates later dates
     plen = len(prices)
-    colour_map = plt.cm.get_cmap(’YlOrRd’)
+    colour_map = plt.cm.get_cmap("YlOrRd")
     colours = np.linspace(0.1, 1, plen)
-    
+
     # Create the scatterplot object
     scatterplot = plt.scatter(
-        prices[etfs[0]], prices[etfs[1]],
-        s=30, c=colours, cmap=colour_map,
-        edgecolor=’k’, alpha=0.8
-        )
-        
+        prices[etfs[0]],
+        prices[etfs[1]],
+        s=30,
+        c=colours,
+        cmap=colour_map,
+        edgecolor="k",
+        alpha=0.8,
+    )
+
     # Add a colour bar for the date colouring and set the
     # corresponding axis tick labels to equal string-formatted dates
     colourbar = plt.colorbar(scatterplot)
     colourbar.ax.set_yticklabels(
-        [str(p.date()) for p in prices[::plen//9].index]
-        )
-        plt.xlabel(prices.columns[0])
-        plt.ylabel(prices.columns[1])
-        plt.show()
-        
-        
+        [str(p.date()) for p in prices[::plen // 9].index]
+    )
+    plt.xlabel(etfs[0])
+    plt.ylabel(etfs[1])
+    plt.show()
+
+
 def calc_slope_intercept_kalman(etfs, prices):
     """
     Utilise the Kalman Filter from the PyKalman package
@@ -49,10 +51,10 @@ def calc_slope_intercept_kalman(etfs, prices):
     """
     delta = 1e-5
     trans_cov = delta / (1 - delta) * np.eye(2)
-    obs_mat = np.vstack(
-        [prices[etfs[0]], np.ones(prices[etfs[0]].shape)]
-    ).T[:, np.newaxis]
-    
+    obs_mat = np.vstack([prices[etfs[0]], np.ones(prices[etfs[0]].shape)]).T[
+              :, np.newaxis
+              ]
+
     kf = KalmanFilter(
         n_dim_obs=1,
         n_dim_state=2,
@@ -61,16 +63,20 @@ def calc_slope_intercept_kalman(etfs, prices):
         transition_matrices=np.eye(2),
         observation_matrices=obs_mat,
         observation_covariance=1.0,
-        transition_covariance=trans_cov
-        )
+        transition_covariance=trans_cov,
+    )
     state_means, state_covs = kf.filter(prices[etfs[1]].values)
     return state_means, state_covs
-    
-    
+
+
 def draw_slope_intercept_changes(prices, state_means):
     """
     Plot the slope and intercept changes from the
     Kalman Filter calculated values.
+
+    Args:
+    - prices (pd.DataFrame): DataFrame containing the prices.
+    - state_means (np.ndarray): Array containing the Kalman Filter state means.
     """
     pd.DataFrame(
         dict(
@@ -79,22 +85,27 @@ def draw_slope_intercept_changes(prices, state_means):
         ), index=prices.index
     ).plot(subplots=True)
     plt.show()
-    
-    
+
+
 if __name__ == "__main__":
     # Choose the ETF symbols to work with along with
     # start and end dates for the price histories
-    etfs = [’TLT’, ’IEI’]
-    start_date = "2010-8-01"
-    end_date = "2016-08-01"
-    
+    etf_symbols = ['TLT', 'IEI']
+    start = "2010-8-01"
+    end = "2016-08-01"
+
     # Obtain the adjusted closing prices from Yahoo finance
-    etf_df1 = pdr.get_data_yahoo(etfs[0], start_date, end_date)
-    etf_df2 = pdr.get_data_yahoo(etfs[1], start_date, end_date)
+    try:
+        etf_df1 = pdr.get_data_yahoo(etf_symbols[0], start, end)
+        etf_df2 = pdr.get_data_yahoo(etf_symbols[1], start, end)
+    except Exception as e:
+        print(f"Error getting data from Yahoo Finance: {e}")
+        exit(1)
+
     prices = pd.DataFrame(index=etf_df1.index)
-    prices[etfs[0]] = etf_df1["Adj Close"]
-    prices[etfs[1]] = etf_df2["Adj Close"]
-    
-    draw_date_coloured_scatterplot(etfs, prices)
-    state_means, state_covs = calc_slope_intercept_kalman(etfs, prices)
+    prices[etf_symbols[0]] = etf_df1["Adj Close"]
+    prices[etf_symbols[1]] = etf_df2["Adj Close"]
+
+    draw_date_coloured_scatterplot(etf_symbols, prices)
+    state_means, state_covs = calc_slope_intercept_kalman(etf_symbols, prices)
     draw_slope_intercept_changes(prices, state_means)
